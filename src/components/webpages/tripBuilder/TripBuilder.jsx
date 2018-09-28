995639739c4f3303713e60b9de04b9c2b2c5df68
Stephen Bulley <stephen.mjbulley@gmail.com>
2018-09-27 19:30:42 -0500

    Intermediate commit

--- src/components/webpages/tripBuilder/TripBuilder.jsx
+++ src/components/webpages/tripBuilder/TripBuilder.jsx
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
 
 import Webpage from "../../webpage/Webpage";
+import { MyMapComponent } from "../../map/Map";
 import "./TripBuilder.css";
 
+const axios = require("axios");
+
 class TripBuilder extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      origin: this.props.origin,
+      destination: this.props.destination,
+      originLat: undefined,
+      originLng: undefined
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://maps.googleapis.com/maps/api/geocode/json", {
+        params: {
+          address: this.state.origin,
+          key: "AIzaSyDg7m2WmdP4yxNFh6NR5weXkk-P91xpmOE"
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .then(function(response) {
+        const origin = response.data.results[0].geometry.location;
+        this.setState({
+          originLat: origin.lat,
+          originLng: origin.lng
+        });
+      });
+  }
+
   render() {
     const content = (
       <div className="trip-builder">
-        {this.props.location.state.origin +
-          " " +
-          this.props.location.state.destination}
+        <MyMapComponent
+          isMarkerShown
+          origin={{ lat: this.state.originLat, lng: this.state.originLng }}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDg7m2WmdP4yxNFh6NR5weXkk-P91xpmOE&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
 
